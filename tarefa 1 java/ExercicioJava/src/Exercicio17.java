5d217b3acf0295b68983eacfafc940d1dfb58f26
Jackson Moraes <jacksontadeu@hotmail.com>
2025-02-26 10:24:58 -0300

    :recycle: refactor: Refatoracao do exercicio 17 e teste de mesa

--- tarefa 1 java/ExercicioJava/src/Exercicio17.java
+++ tarefa 1 java/ExercicioJava/src/Exercicio17.java
@@ -14,26 +14,33 @@ public class Exercicio17 {
         double e2 = input.nextDouble();
 
         double media = ((p1*0.6)+ ((e1 + e2 )/ 2 * 0.4)) * 0.5;
-        System.out.println(media);
-        double max = Math.max(((p1*0.6)+ ((e1 + e2 )/ 2 * 0.4))- 5.9,0);
-        System.out.println(max);
-        double fator =  max / (((p1*0.6)+ ((e1 + e2 )/ 2 * 0.4))- 5.9);
-        System.out.println(fator);
 
-        double api, mediaFinal, sub;
+        double max = Math.max(((p1*0.6)+ ((e1 + e2 )/ 2 * 0.4))- 5.9,0)/
+                (((p1*0.6)+ ((e1 + e2 )/ 2 * 0.4))- 5.9);
 
-        if(fator ==1){
+        double api, mediaFinal, sub, pontosExtras;
+
+        if(max ==1){
             System.out.print("Nota API: ");
             api = input.nextDouble();
-            mediaFinal = media + (api * 0.5);
-            System.out.println(mediaFinal);
-
+            System.out.print("Pontos Extras: ");
+            pontosExtras = input.nextDouble();
+            mediaFinal = media + (api * 0.5) + pontosExtras;
+            if (mediaFinal < 6){
+                System.out.print("Nota SUB: ");
+                sub = input.nextDouble();
+                mediaFinal += (sub * 0.3);
+                System.out.printf("Média final: %.1f",mediaFinal);
+            }else{
+                System.out.printf("Média final: %.1f",mediaFinal);
+            }
         }else{
             System.out.print("Nota SUB: ");
             sub = input.nextDouble();
-            mediaFinal = media + (sub * 0.3);
-            System.out.println(mediaFinal);
-
+            System.out.print("Pontos Extras: ");
+            pontosExtras = input.nextDouble();
+            mediaFinal = media + (sub * 0.3) + pontosExtras;
+            System.out.printf("Media Final: %.1f",mediaFinal);
         }
     }
 }
